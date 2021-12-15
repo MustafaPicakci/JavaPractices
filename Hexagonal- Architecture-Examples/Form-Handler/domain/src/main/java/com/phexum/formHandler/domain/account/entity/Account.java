62abcfb073fb2765c455280fcb8aaeddcaa2eccf
Mustafa Pıçakçı <mustafapicakci25@gmail.com>
2021-12-15 14:13:00 +0300

    form-handler silindi

--- Hexagonal- Architecture-Examples/Form-Handler/domain/src/main/java/com/phexum/formHandler/domain/account/entity/Account.java
+++
@@ -1,4 +0,0 @@
-package com.phexum.formHandler.domain.account.entity;
-
-public class Account {
-}
--- Hexagonal- Architecture-Examples/Form-Handler/domain/src/main/java/com/phexum/formHandler/domain/feature/Feature.java
+++
@@ -1,5 +0,0 @@
-package com.phexum.formHandler.domain.feature;
-
-public enum Feature {
-    SPAM, REPORT, AUTO_REPLY, LIMIT_5, LIMIT_10, LIMITLESS, MAX_FORM_TYPE_2, MAX_FORM_TYPE_5, EXPORT, INSTANT_ALERT
-}
